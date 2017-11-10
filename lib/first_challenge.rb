ed51360ae55927a1aff1ebd75e7f3cf21304dfa6
Cristy Lucke <cristy.lucke@gmail.com>
2017-11-10 19:20:16 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
   }
 
   contacts.each do |person, data|
-    if person == "Freddy"
+    if person == "Freddy Mercury"
       data.each do |attribute, value|
         if attribute == :favorite_icecream_flavors
           flavors = "The attribute is #{attribute} and value #{value}"
