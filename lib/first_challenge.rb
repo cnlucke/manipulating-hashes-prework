f25d0e22719c5a5641221649d2e883a927062dd7
Cristy Lucke <cristy.lucke@gmail.com>
2017-11-10 18:59:00 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,9 +14,11 @@ def first_challenge
   }
 
   contacts.each do |person, data|
-    data.each do |attribute, value|
-      if attribute == :favorite_icecream_flavors
-        value.delete("strawberry")
+    if person == "Freddy"
+      data.each do |attribute, value|
+        if attribute == :favorite_icecream_flavors
+          value.delete("strawberry")
+        end
       end
     end
   end
